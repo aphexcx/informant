526821d2092a2e7f183c176b6efafbbaaac06ca1
Afik Cohen <aphex@aphex.cx>
2017-01-16 21:56:32 -0800

    Bind to PORT environment variable.

--- akkahttp/src/main/scala/runDebug.scala
+++ akkahttp/src/main/scala/runDebug.scala
@@ -6,8 +6,9 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import co.datamonsters.facebot._
-import co.datamonsters.facebot.api.{Message, Messaging}
 import co.datamonsters.facebot.api.NotificationType.Regular
+import co.datamonsters.facebot.api.{Message, Messaging}
+
 import scala.io.StdIn
 
 object runDebug {
@@ -16,6 +17,8 @@ object runDebug {
     implicit val system = ActorSystem("facebotActorSystem")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
+
+    val port: Int = Option(System.getenv("PORT")).map(_.toInt).getOrElse(8701)
 
     val facebotConnection = AkkaHttpConnection(Credentials(
       "EAACXksYN6lgBAFVECWFZCLONZAZBr4MP8g55ne6JIqZBPog7SubQYZANKIj6ZAC7CJ9jZA7h3ixSlTvKKCyHkTuwRJlVZAS5Sh9SZCsf8daqvj6o3ZCpXMk49fuLwqm8ZBZAxS0VceBDUPD6iPjgckii6ziXECy2RGPPMKjZBF1RD78flWwZDZD",
@@ -26,7 +29,8 @@ object runDebug {
     }
 
     val bindingFuture =
-      Http().bindAndHandle(facebotConnection.facebookRestRoute, "localhost", 8701)
+      Http().bindAndHandle(facebotConnection.facebookRestRoute, "localhost", port)
+    println(s"Listening on port $port!")
     StdIn.readLine() // let it run until user presses return
     bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
 
